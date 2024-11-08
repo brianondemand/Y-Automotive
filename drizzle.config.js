db6e5f1952bf691f2a7960ca17aa339318e3faa0
brianondemand <snowworksdev@gmail.com>
2024-11-08 20:25:33 +0300

    Security Changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-undef */
 /** @type { import("drizzle-kit").Config} */
-export default{
-    schema: './configs/schema.js',
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://neondb_owner:cT8qbBuWo5RP@ep-super-wind-a5h8a81g.us-east-2.aws.neon.tech/automotive_db?sslmode=require',
-    },
-  };
+import 'dotenv/config';
+
+export default {
+  schema: './configs/schema.js',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+};
